b0078cd257d119c8804ed873a921f36494dc4322
Aronaks <homeworks00@mail.ru>
2024-01-05 14:29:27 +0300

    Commit 2.

--- main.py
+++ main.py
@@ -68,6 +68,7 @@ images = {
     "land": pygame.transform.scale(load_image('land.png'), (50, 50)),
     "door": pygame.transform.scale(load_image('door.png'), (75, 125))
 }
+player_image = pygame.transform.scale(load_image('player.png'), (150, 125))
 
 tile_width = tile_height = 50
 
@@ -80,8 +81,40 @@ class Tile(pygame.sprite.Sprite):
             tile_width * pos_x, tile_height * pos_y)
 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(tile_width, tile_height + 790)
+        self.jmp = 0
+        self.gravity = 0
+
+    def update(self):
+        key = pygame.key.get_pressed()
+        if key[pygame.K_UP] and self.jmp == 0:
+            self.gravity = -12
+            self.jmp = 1
+        if not key[pygame.K_UP]:
+            self.jmp = 0
+        if key[pygame.K_RIGHT]:
+            self.rect.x += 4
+        if key[pygame.K_LEFT]:
+            self.rect.x -= 4
+
+        self.gravity += 1
+        if self.gravity > 8:
+            self.gravity = 8
+        self.rect.y += self.gravity
+
+        if self.rect.bottom > height - 35:
+            self.rect.bottom = height - 35
+
+
+player = None
+
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
 
 
 def generate_level(level):
@@ -94,11 +127,13 @@ def generate_level(level):
                 Tile('grass', x, y)
             elif level[y][x] == '$':
                 Tile('door', x - 0.5, y - 1.5)
+            elif level[y][x] == "&":
+                new_player = Player(x, y)
     # вернем игрока, а также размер поля в клетках
-    return x, y
+    return new_player, x, y
 
 
-level_x, level_y = generate_level(load_level('lvl1.txt'))
+player, level_x, level_y = generate_level(load_level('lvl1.txt'))
 
 running = True
 while running:
@@ -110,6 +145,7 @@ while running:
     screen.blit(fon, (0, 0))
     all_sprites.draw(screen)
     tiles_group.draw(screen)
+    player_group.draw(screen)
     all_sprites.update()
     clock.tick(60)
     pygame.display.flip()
