f748041353e6618d9b6c47e08cd94849c2d1048c
Aronaks <homeworks00@mail.ru>
2024-01-09 21:32:54 +0300

    Commit 3. (переключение лвлов)

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import os
 import sys
 import pygame
 
+
 pygame.init()
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
-
+lvl = 1
+k = 0
 
 def load_image(name):
     fullname = os.path.join('data', name)
@@ -62,13 +64,12 @@ def load_level(filename):
     return list(map(lambda x: x.ljust(max_width, '.'), level_map))
 
 
-level = load_level("lvl1.txt")
 images = {
-    "grass": pygame.transform.scale(load_image('grass.png'), (50, 50)),
     "land": pygame.transform.scale(load_image('land.png'), (50, 50)),
+    "grass": pygame.transform.scale(load_image('grass.png'), (50, 50)),
     "door": pygame.transform.scale(load_image('door.png'), (75, 125))
 }
-player_image = pygame.transform.scale(load_image('player.png'), (150, 125))
+player_image = pygame.transform.scale(load_image('player.png'), (100, 100))
 
 tile_width = tile_height = 50
 
@@ -85,11 +86,16 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__(player_group, all_sprites)
         self.image = player_image
-        self.rect = self.image.get_rect().move(tile_width, tile_height + 790)
+        self.rect = self.image.get_rect().move(tile_width, tile_height + 800)
         self.jmp = 0
         self.gravity = 0
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+        self.dx = 0
+        self.k = 1
 
     def update(self):
+        global player, level_x, level_y, land_list, exit_dr, k, lvl
         key = pygame.key.get_pressed()
         if key[pygame.K_UP] and self.jmp == 0:
             self.gravity = -12
@@ -97,17 +103,39 @@ class Player(pygame.sprite.Sprite):
         if not key[pygame.K_UP]:
             self.jmp = 0
         if key[pygame.K_RIGHT]:
-            self.rect.x += 4
+            self.dx = 4
         if key[pygame.K_LEFT]:
-            self.rect.x -= 4
+            self.dx = -4
 
         self.gravity += 1
         if self.gravity > 8:
             self.gravity = 8
         self.rect.y += self.gravity
 
-        if self.rect.bottom > height - 35:
-            self.rect.bottom = height - 35
+        for tile in land_list:
+            if tile.colliderect(self.rect.x + self.dx, self.rect.y - 1, self.width, self.height):
+                self.dx = 0
+            if tile.colliderect(self.rect.x, self.rect.y + self.gravity, self.width, self.height):
+                if self.gravity < 0:
+                    self.rect.y += (tile.bottom - self.rect.top)
+                    self.gravity = 0
+                elif self.gravity >= 0:
+                    self.rect.y += (tile.top - self.rect.bottom)
+                    self.gravity = 0
+        for move in exit_dr:
+            if move.colliderect(self.rect.x, self.rect.y + self.gravity, self.width, self.height):
+                all_sprites.empty()
+                tiles_group.empty()
+                player_group.empty()
+                k = 1
+                if k == 1:
+                    lvl += 1
+                    k = 0
+                player, level_x, level_y, land_list, exit_dr = generate_level(load_level(f"lvl{lvl}.txt"))
+        self.rect.x += self.dx
+        if not key[pygame.K_LEFT] or not key[pygame.K_RIGHT]:
+            self.dx = 0
+
 
 
 player = None
@@ -115,25 +143,41 @@ player = None
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
+land_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
 
 
 def generate_level(level):
     new_player, x, y = None, None, None
+    land_list = []
+    exit_dr = []
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
-                Tile('land', x, y)
+                image = images["land"]
+                rect = image.get_rect().move(
+                    tile_width * x, tile_height * y)
+                land_list.append(rect)
+                Tile("land", x, y)
             elif level[y][x] == '@':
                 Tile('grass', x, y)
+                image = images["grass"]
+                rect = image.get_rect().move(
+                    tile_width * x, tile_height * y)
+                land_list.append(rect)
             elif level[y][x] == '$':
                 Tile('door', x - 0.5, y - 1.5)
+                image = images["door"]
+                rect = image.get_rect().move(tile_width * x, tile_height * y)
+                exit_dr.append(rect)
             elif level[y][x] == "&":
                 new_player = Player(x, y)
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
+    return new_player, x, y, land_list, exit_dr
 
 
-player, level_x, level_y = generate_level(load_level('lvl1.txt'))
+player, level_x, level_y, land_list, exit_dr = generate_level(load_level('lvl1.txt'))
+
 
 running = True
 while running:
