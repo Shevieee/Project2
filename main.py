f15c1eb0498f962c7ad0350573ec81db8b1f4f81
Aronaks <homeworks00@mail.ru>
2024-01-12 23:36:46 +0300

    Commit 4. (препятствия, уровни(6/10), смерть перса, кнопка рестарт)

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import os
 import sys
 import pygame
 
-
 pygame.init()
 size = width, height = 1000, 1000
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 lvl = 1
 k = 0
+score = 0
+dead = 0
+
 
 def load_image(name):
     fullname = os.path.join('data', name)
@@ -25,9 +27,6 @@ def terminate():
 
 
 def start_screen():
-    intro_text = ["", "",
-                  ""]
-
     fon = pygame.transform.scale(load_image('background.jpg'), (1000, 1000))
     screen.blit(fon, (0, 0))
     start_btn = pygame.transform.scale(load_image('start_btn.png'), (279, 126))
@@ -67,10 +66,13 @@ def load_level(filename):
 images = {
     "land": pygame.transform.scale(load_image('land.png'), (50, 50)),
     "grass": pygame.transform.scale(load_image('grass.png'), (50, 50)),
-    "door": pygame.transform.scale(load_image('door.png'), (75, 125))
+    "door": pygame.transform.scale(load_image('door.png'), (75, 125)),
+    "cactus": pygame.transform.scale(load_image("cactus.png"), (50, 50))
 }
-player_image = pygame.transform.scale(load_image('player.png'), (100, 100))
-
+player_image = pygame.transform.scale(load_image('player.png'), (98, 98))
+coin_image = pygame.transform.scale(load_image('coin.png'), (50, 50))
+restart_btn = pygame.transform.scale(load_image("retry_btn.png"), (150, 50))
+dead_player = pygame.transform.scale(load_image("dead.png"), (100, 50))
 tile_width = tile_height = 50
 
 
@@ -78,6 +80,22 @@ class Tile(pygame.sprite.Sprite):
     def __init__(self, tile_type, pos_x, pos_y):
         super().__init__(tiles_group, all_sprites)
         self.image = images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Coin(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(coins_group, all_sprites)
+        self.image = coin_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+class Cactuss(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(cactus_group, all_sprites)
+        self.image = coin_image
         self.rect = self.image.get_rect().move(
             tile_width * pos_x, tile_height * pos_y)
 
@@ -95,10 +113,10 @@ class Player(pygame.sprite.Sprite):
         self.k = 1
 
     def update(self):
-        global player, level_x, level_y, land_list, exit_dr, k, lvl
+        global player, level_x, level_y, land_list, exit_dr, k, dead, lvl, score, cactus
         key = pygame.key.get_pressed()
         if key[pygame.K_UP] and self.jmp == 0:
-            self.gravity = -12
+            self.gravity = -16
             self.jmp = 1
         if not key[pygame.K_UP]:
             self.jmp = 0
@@ -123,19 +141,43 @@ class Player(pygame.sprite.Sprite):
                     self.rect.y += (tile.top - self.rect.bottom)
                     self.gravity = 0
         for move in exit_dr:
-            if move.colliderect(self.rect.x, self.rect.y + self.gravity, self.width, self.height):
+            if move.colliderect(self.rect.x, self.rect.y, self.width, self.height):
                 all_sprites.empty()
                 tiles_group.empty()
                 player_group.empty()
+                coins_group.empty()
                 k = 1
                 if k == 1:
                     lvl += 1
                     k = 0
-                player, level_x, level_y, land_list, exit_dr = generate_level(load_level(f"lvl{lvl}.txt"))
+                player, level_x, level_y, land_list, exit_dr, cactus = generate_level(load_level(f"lvl{lvl}.txt"))
+        for coin in coins_group:
+            if self.rect.colliderect(coin.rect):
+                score += 10
+                coin.kill()
+        for cact in cactus:
+            if cact.colliderect(self.rect.x, self.rect.y, self.width, self.height):
+                self.image = dead_player
+                self.jmp = -1
+                self.dx = 0
+                dead = -1
+                screen.blit(restart_btn, (400, 500))
+
         self.rect.x += self.dx
         if not key[pygame.K_LEFT] or not key[pygame.K_RIGHT]:
             self.dx = 0
 
+        score_text = [f"СЧЁТ: {score}"]
+
+        font = pygame.font.Font(None, 30)
+        text_coord = 10
+        for line in score_text:
+            string_rendered = font.render(line, 1, pygame.Color('black'))
+            score_rect = string_rendered.get_rect()
+            score_rect.top = text_coord
+            score_rect.x = 51
+            score_rect.y = 51
+            screen.blit(string_rendered, score_rect)
 
 
 player = None
@@ -143,14 +185,16 @@ player = None
 all_sprites = pygame.sprite.Group()
 tiles_group = pygame.sprite.Group()
 player_group = pygame.sprite.Group()
-land_group = pygame.sprite.Group()
 exit_group = pygame.sprite.Group()
+coins_group = pygame.sprite.Group()
+cactus_group = pygame.sprite.Group()
 
 
 def generate_level(level):
     new_player, x, y = None, None, None
     land_list = []
     exit_dr = []
+    cactus = []
     for y in range(len(level)):
         for x in range(len(level[y])):
             if level[y][x] == '.':
@@ -172,24 +216,41 @@ def generate_level(level):
                 exit_dr.append(rect)
             elif level[y][x] == "&":
                 new_player = Player(x, y)
+            elif level[y][x] == "%":
+                Coin(x, y)
+            elif level[y][x] == "*":
+                Tile("cactus", x, y)
+                image = images["cactus"]
+                rect = image.get_rect().move(tile_width * x, tile_height * y)
+                cactus.append(rect)
+
     # вернем игрока, а также размер поля в клетках
-    return new_player, x, y, land_list, exit_dr
+    return new_player, x, y, land_list, exit_dr, cactus
 
 
-player, level_x, level_y, land_list, exit_dr = generate_level(load_level('lvl1.txt'))
-
+player, level_x, level_y, land_list, exit_dr, cactus = generate_level(load_level('lvl1.txt'))
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN and dead == -1:
+            x, y = event.pos
+            if (401 <= x <= 549) and (500 <= y <= 550):
+                all_sprites.empty()
+                tiles_group.empty()
+                player_group.empty()
+                coins_group.empty()
+                player, level_x, level_y, land_list, exit_dr, cactus = generate_level(load_level(f"lvl{lvl}.txt"))
     screen.fill(pygame.Color("white"))
     fon = pygame.transform.scale(load_image('background.jpg'), (1000, 1000))
     screen.blit(fon, (0, 0))
     all_sprites.draw(screen)
     tiles_group.draw(screen)
     player_group.draw(screen)
+    exit_group.draw(screen)
+    coins_group.draw(screen)
     all_sprites.update()
     clock.tick(60)
     pygame.display.flip()
